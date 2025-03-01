9f8be2d20ca5cdc1b64a7b4d62088a64064a022e
noy4 <naoyan418@gmail.com>
2025-03-01 11:19:11 +0900

    NoteTitle

---
+++ docs/.vitepress/theme/components/NoteTitle.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useData } from 'vitepress'
+import { computed } from 'vue'
+
+const { page } = useData()
+
+const title = computed(() =>
+  page.value.title
+  || page.value.filePath
+    .split('/')
+    .pop()
+    .replace(/\.md$/, ''),
+)
+</script>
+
+<template>
+  <div class="vp-doc">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,16 @@
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import NoteTitle from './components/NoteTitle.vue'
 import 'virtual:uno.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': () => {
+        return h(NoteTitle)
+      },
+    })
+  },
+} satisfies Theme
