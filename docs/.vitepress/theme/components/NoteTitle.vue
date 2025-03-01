4ddea7a30dbb638e7745c209ebe93a0bc02db5ec
noy4 <naoyan418@gmail.com>
2025-03-01 12:54:14 +0900

    page.title

--- docs/.vitepress/theme/components/NoteTitle.vue
+++ docs/.vitepress/theme/components/NoteTitle.vue
@@ -1,20 +1,11 @@
 <script setup>
 import { useData } from 'vitepress'
-import { computed } from 'vue'
 
 const { page } = useData()
-
-const title = computed(() =>
-  page.value.title
-  || page.value.filePath
-    .split('/')
-    .pop()
-    .replace(/\.md$/, ''),
-)
 </script>
 
 <template>
   <div class="vp-doc">
-    <h1>{{ title }}</h1>
+    <h1>{{ page.title }}</h1>
   </div>
 </template>
