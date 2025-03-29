42e7cf579698062f042cd5f04a5bcb8323991841
noy4 <naoyan418@gmail.com>
2025-03-29 12:11:20 +0900

    quest section

--- docs/.vitepress/components/QuestView.vue
+++ docs/.vitepress/components/QuestView.vue
@@ -4,11 +4,17 @@ import QuestCard from './QuestCard.vue'
 </script>
 
 <template>
-  <div class="grid sm:cols-2 gap-2 mt-4">
-    <QuestCard
-      v-for="quest in data"
-      :key="quest.title"
-      :quest="quest"
-    />
+  <div v-for="section in data" :key="section.title">
+    <div class="text-2xl font-semibold mt-8 mb-4">
+      {{ section.title }}
+    </div>
+
+    <div class="grid sm:cols-2 gap-2 mt-4">
+      <QuestCard
+        v-for="quest in section.items"
+        :key="quest.title"
+        :quest="quest"
+      />
+    </div>
   </div>
 </template>
--- docs/.vitepress/theme/quest.data.ts
+++ docs/.vitepress/theme/quest.data.ts
@@ -1,5 +1,10 @@
 import { createContentLoader } from 'vitepress'
 
+export interface Section {
+  title: string
+  items: Quest[]
+}
+
 export interface Quest {
   icon?: string
   title: string
@@ -7,30 +12,53 @@ export interface Quest {
   description?: string
 }
 
-declare const data: Quest[]
+declare const data: Section[]
 export { data }
 
 export default createContentLoader('+quest.md', {
   includeSrc: true,
   transform(data) {
     const src = data[0]?.src || ''
-    const items = src.split(/\n\n\n/).map(v => v.trim())
+
+    // ## で始まるセクションで分割
     // 1行目：タイトル
     // 2行目：ターゲット
     // 3行目以降：説明
-    return items.map((item) => {
-      const lines = item.split(/\n/)
-      const [first, target, ...rest] = lines
-      const [icon, ..._title] = first.split(/\s+/)
-      const title = _title.join(' ')
-      const description = rest.join('\n')
 
-      return {
-        icon,
-        title,
-        target,
-        description,
-      }
-    })
+    const sections = src
+      .split(/(?=^##)/m)
+      .slice(1)
+      .map((section) => {
+        const [sectionTitle, ..._sectionContent] = section.split(/\n/)
+
+        const items = _sectionContent
+          .join('\n')
+          .trim()
+          .split(/\n\n\n/)
+          .map(v => v.trim())
+          .map((item) => {
+            const itemLines = item.split(/\n/)
+
+            const [_title, target, ..._description] = itemLines
+            const [icon, ...__title] = _title
+              .replace(/^## /, '')
+              .split(/\s+/)
+            const title = __title.join(' ')
+            const description = _description.join('\n')
+            return {
+              icon,
+              title,
+              target,
+              description,
+            }
+          })
+
+        return {
+          title: sectionTitle.replace(/^## /, ''),
+          items,
+        }
+      })
+
+    return sections
   },
 })
