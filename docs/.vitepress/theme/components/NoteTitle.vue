b1287c2a114f5128afdc007dfd1d1b4e94fc7417
noy4 <naoyan418@gmail.com>
2025-04-26 04:41:12 +0900

    !hasH1

--- docs/.vitepress/theme/components/NoteTitle.vue
+++ docs/.vitepress/theme/components/NoteTitle.vue
@@ -1,11 +1,29 @@
 <script setup>
-import { useData } from 'vitepress'
+import { useData, useRoute } from 'vitepress'
+import { nextTick, onMounted, ref, watch } from 'vue'
 
 const { page } = useData()
+const route = useRoute()
+const hasH1 = ref(true)
+
+function checkH1() {
+  hasH1.value = !!document.querySelector('h1')
+}
+
+onMounted(checkH1)
+
+watch(() => route.path, async () => {
+  hasH1.value = true
+  await nextTick()
+  checkH1()
+})
 </script>
 
 <template>
-  <div class="vp-doc">
+  <div
+    v-if="!hasH1 && page.title"
+    class="vp-doc"
+  >
     <h1>{{ page.title }}</h1>
   </div>
 </template>
