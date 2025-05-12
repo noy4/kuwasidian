f68f6c415fccc0f2d5a243d0d933ca385b54cb94
noy4 <naoyan418@gmail.com>
2025-05-12 20:14:52 +0900

    utils

--- docs/.vitepress/utils.ts
+++ docs/.vitepress/utils.ts
@@ -15,3 +15,11 @@ export function extractDescription(content: string): string {
     ? `${escapedText.slice(0, 120)}...`
     : escapedText
 }
+
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString)
+  const year = date.getFullYear()
+  const month = `0${date.getMonth() + 1}`.slice(-2)
+  const day = `0${date.getDate()}`.slice(-2)
+  return `${year}/${month}/${day}`
+}
