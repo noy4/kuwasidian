3b716934251998299ed90dd3f96f019657c9a852
noy4 <naoyan418@gmail.com>
2025-05-19 20:04:48 +0900

    parseQuestData

--- docs/quests/quest.data.ts
+++ docs/quests/quest.data.ts
@@ -29,51 +29,55 @@ export default createContentLoader('quests/quest.data.md', {
   includeSrc: true,
   async transform(data) {
     const src = data[0]?.src || ''
-    const md = await createMd()
-
-    // ## で始まるセクションで分割
-    // 1行目：タイトル
-    // 2行目：ターゲット
-    // 3行目以降：説明
-
-    const sections = src
-      .split(/(?=^##)/m)
-      .map((section, sectionIdx) => {
-        let sectionTitle = ''
-        let sectionContent = section
-
-        if (section.startsWith('## ')) {
-          const chunks = section.split(/\n/)
-          sectionTitle = chunks[0].replace(/^## /, '')
-          sectionContent = chunks.slice(1).join('\n')
-        }
-
-        const items = sectionContent
-          .trim()
-          .split(/\n\n\n/)
-          .map(v => v.trim())
-          .map((item) => {
-            const itemLines = item.split(/\n/)
-            const [_title, target, ..._description] = itemLines
-            const [icon, ...__title] = _title.split(/\s+/)
-            const title = __title.join(' ')
-            const description = md.render(_description.join('\n'))
-
-            return {
-              icon,
-              title,
-              target,
-              description,
-              status: statusMap[sectionIdx],
-            }
-          })
-
-        return {
-          title: sectionTitle,
-          items,
-        }
-      })
-
-    return sections
+    return parseQuestData(src)
   },
 })
+
+export async function parseQuestData(src: string): Promise<Section[]> {
+  const md = await createMd()
+
+  // ## で始まるセクションで分割
+  // 1行目：タイトル
+  // 2行目：ターゲット
+  // 3行目以降：説明
+
+  const sections = src
+    .split(/(?=^##)/m)
+    .map((section, sectionIdx) => {
+      let sectionTitle = ''
+      let sectionContent = section
+
+      if (section.startsWith('## ')) {
+        const chunks = section.split(/\n/)
+        sectionTitle = chunks[0].replace(/^## /, '')
+        sectionContent = chunks.slice(1).join('\n')
+      }
+
+      const items = sectionContent
+        .trim()
+        .split(/\n\n\n/)
+        .map(v => v.trim())
+        .map((item) => {
+          const itemLines = item.split(/\n/)
+          const [_title, target, ..._description] = itemLines
+          const [icon, ...__title] = _title.split(/\s+/)
+          const title = __title.join(' ')
+          const description = md.render(_description.join('\n'))
+
+          return {
+            icon,
+            title,
+            target,
+            description,
+            status: statusMap[sectionIdx],
+          }
+        })
+
+      return {
+        title: sectionTitle,
+        items,
+      }
+    })
+
+  return sections
+}
