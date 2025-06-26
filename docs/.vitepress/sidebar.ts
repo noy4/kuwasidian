1238aa807ce9167864547caa2ddabeea51ca2eed
noy4 <naoyan418@gmail.com>
2025-06-27 01:07:05 +0900

    sidebar.ts

---
+++ docs/.vitepress/sidebar.ts
@@ -0,0 +1,40 @@
+import type { DefaultTheme } from 'vitepress'
+import type { VitePressSidebarOptions } from 'vitepress-sidebar/types'
+import process from 'node:process'
+import { generateSidebar } from 'vitepress-sidebar'
+import { defaultExcludePattern } from './utils.server'
+
+export function autoSidebar(
+  folderPath: string,
+  options?: VitePressSidebarOptions & {
+    desc?: boolean
+  },
+) {
+  const { desc, ...opts } = options || {}
+
+  const generated = generateSidebar({
+    documentRootPath: process.argv[3],
+    scanStartPath: folderPath,
+    useFolderLinkFromIndexFile: true,
+    sortMenusByName: true,
+    sortMenusOrderByDescending: desc,
+    collapsed: true,
+    excludePattern: defaultExcludePattern,
+    ...opts,
+  }) as DefaultTheme.SidebarItem[]
+
+  return addBase(generated, `/${folderPath}/`)
+}
+
+type SidebarItem = DefaultTheme.SidebarItem
+
+function addBase(items: SidebarItem[], base: string): SidebarItem[] {
+  return [...items].map((_item) => {
+    const item = { ..._item }
+    if (item.link)
+      item.link = base + item.link
+    if (item.items)
+      item.items = addBase(item.items, base)
+    return item
+  })
+}
--- docs/.vitepress/utils.server.ts
+++ docs/.vitepress/utils.server.ts
@@ -1,8 +1,4 @@
-import type { DefaultTheme } from 'vitepress'
-import type { VitePressSidebarOptions } from 'vitepress-sidebar/types'
 import fs from 'node:fs'
-import process from 'node:process'
-import { generateSidebar } from 'vitepress-sidebar'
 
 function parseGitignore(filePath = '.gitignore') {
   return fs.readFileSync(filePath, 'utf-8')
@@ -12,38 +8,3 @@ function parseGitignore(filePath = '.gitignore') {
 }
 
 export const defaultExcludePattern = parseGitignore()
-
-export function autoSidebar(
-  folderPath: string,
-  options?: VitePressSidebarOptions & {
-    desc?: boolean
-  },
-) {
-  const { desc, ...opts } = options || {}
-
-  const generated = generateSidebar({
-    documentRootPath: process.argv[3],
-    scanStartPath: folderPath,
-    useFolderLinkFromIndexFile: true,
-    sortMenusByName: true,
-    sortMenusOrderByDescending: desc,
-    collapsed: true,
-    excludePattern: defaultExcludePattern,
-    ...opts,
-  }) as DefaultTheme.SidebarItem[]
-
-  return addBase(generated, `/${folderPath}/`)
-
-  type SidebarItem = DefaultTheme.SidebarItem
-
-  function addBase(items: SidebarItem[], base: string): SidebarItem[] {
-    return [...items].map((_item) => {
-      const item = { ..._item }
-      if (item.link)
-        item.link = base + item.link
-      if (item.items)
-        item.items = addBase(item.items, base)
-      return item
-    })
-  }
-}
