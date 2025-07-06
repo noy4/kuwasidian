25b453c4a692b68fc851eb5b3eb566ea09e86889
noy4 <naoyan418@gmail.com>
2025-07-07 03:26:31 +0900

    split EarthCard

---
+++ docs/earth/city-history/EarthCard.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import type { Earth } from './Earth'
+
+defineProps<{
+  earth: Earth
+}>()
+</script>
+
+<template>
+  <div class="absolute left-2 top-2 bg-black/70 rounded p-3">
+    <h3 class="text-white text-lg font-bold mb-2">
+      都市履歴
+    </h3>
+
+    <ul class="timeline timeline-vertical">
+      <li v-for="(city, index) in earth.cities" :key="index">
+        <hr v-if="index !== 0" class="bg-gray-600">
+
+        <div
+          class="timeline-start text-sm"
+          :class="[
+            earth.currentCityIndex.value === index
+              ? 'text-base-100'
+              : 'text-gray-400',
+          ]"
+        >
+          {{ city.year }}
+        </div>
+
+        <div class="timeline-middle">
+          <div
+            class="i-tabler-circle-check-filled w-4 h-4 cursor-pointer transition-colors duration-200"
+            :class="[
+              earth.currentCityIndex.value === index
+                ? 'bg-green-500'
+                : 'bg-gray-400 hover:bg-gray-300',
+            ]"
+            @click="earth.goToCity(index)"
+          />
+        </div>
+
+        <button
+          class="timeline-end text-sm transition-colors duration-200"
+          :class="[
+            earth.currentCityIndex.value === index
+              ? 'text-white'
+              : 'text-gray-400 hover:text-gray-300',
+          ]"
+          @click="earth.goToCity(index)"
+        >
+          {{ city.name }}
+        </button>
+
+        <hr v-if="index < earth.cities.length - 1" class="bg-gray-600">
+      </li>
+    </ul>
+
+    <button
+      class="btn btn-primary mt-4 w-full"
+      @click="earth.moveToNextCity()"
+    >
+      次の都市へ
+    </button>
+
+    <button
+      class="btn mt-2 w-full"
+      :class="earth.isRotating.value ? 'btn-error' : 'btn-success'"
+      @click="earth.toggleCameraRotation()"
+    >
+      {{ earth.isRotating.value ? '回転停止' : '回転開始' }}
+    </button>
+  </div>
+</template>
