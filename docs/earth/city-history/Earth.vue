37755ecb58accb5e4c97a10758aedcd52985cc87
noy4 <naoyan418@gmail.com>
2025-07-06 20:59:00 +0900

    city

--- docs/earth/city-history/Earth.vue
+++ docs/earth/city-history/Earth.vue
@@ -5,12 +5,12 @@ import { onMounted } from 'vue'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 
 const cities = [
-  { year: 1998, name: '広島・福山', longitude: 133.3675482937462, latitude: 34.48143558148611 },
-  { year: 1998, name: '埼玉・大宮', longitude: 139.61808580282548, latitude: 35.94521419848559 },
-  { year: 2005, name: '島根・松江', longitude: 133.0722218545677, latitude: 35.475048093578025 },
-  { year: 2008, name: '大阪・枚方', longitude: 135.6670453481949, latitude: 34.85421820595925 },
-  { year: 2011, name: '愛媛・松山', longitude: 132.78038862244543, latitude: 33.84173274937172 },
-  { year: 2017, name: '福岡・大橋', longitude: 130.43078806765084, latitude: 33.561114860244885 },
+  { year: 1998, name: '広島・福山', longitude: 133.36709945519166, latitude: 34.48121901629059 },
+  { year: 1998, name: '埼玉・大宮', longitude: 139.61697096710148, latitude: 35.94446389018784 },
+  { year: 2005, name: '島根・松江', longitude: 133.07209744986676, latitude: 35.47512971396031 },
+  { year: 2008, name: '大阪・枚方', longitude: 135.664640267663, latitude: 34.83626989287535 },
+  { year: 2011, name: '愛媛・松山', longitude: 132.780117639121, latitude: 33.84188377853939 },
+  { year: 2017, name: '福岡・大橋', longitude: 130.43027813106573, latitude: 33.56095413278243 },
 ]
 
 let viewer: Cesium.Viewer
