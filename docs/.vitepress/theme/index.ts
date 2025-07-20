465fa29a9c5e28af8fc37d669ca47a6552b81877
noy4 <naoyan418@gmail.com>
2025-07-21 01:17:30 +0900

    rucksack

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,6 @@
+// [Extending the Default Theme | VitePress](https://vitepress.dev/guide/extending-default-theme)
 import DefaultTheme from 'vitepress/theme'
 import 'virtual:uno.css'
+import './custom.css'
 
 export default DefaultTheme
