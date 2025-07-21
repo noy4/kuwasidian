417036fb5f26148e0b03df1140e035fe0d0b9286
noy4 <naoyan418@gmail.com>
2025-07-22 05:09:45 +0900

    insertH1IfMissing with md.render

--- docs/.vitepress/markdown.ts
+++ docs/.vitepress/markdown.ts
@@ -22,6 +22,8 @@ export function createMd() {
   )
 }
 
+// --- plugins ---
+
 export function wikilinks() {
   return BiDirectionalLinks({
     dir: process.argv[3],
@@ -31,41 +33,25 @@ export function wikilinks() {
 // [title · Issue #4629 · vuejs/vitepress](https://github.com/vuejs/vitepress/issues/4629)
 export function insertH1IfMissing() {
   return (md: MarkdownRenderer) => {
-    md.core.ruler.after('block', 'insert_h1_if_missing', (state) => {
-      const { env, tokens, Token } = state
+    const originalRender = md.render.bind(md)
 
+    md.render = function (src, env?: MarkdownEnv) {
+      const { data, content } = matter(src)
       if (
-        env.h1Handled
-        || !env.path
-        || (env.frontmatter.layout && env.frontmatter.layout !== 'doc')
+        env
+        && (!data.layout || data.layout === 'doc') // 'doc' layout
+        && env.relativePath !== 'index.md' // not in index.md
       ) {
-        return
+        const hasH1 = content.startsWith('# ')
+        if (!hasH1) {
+          const fileName = path.basename(env.path, path.extname(env.path))
+          const title = data.title || fileName
+          src = matter.stringify(`# ${title}\n\n${content}`, data)
+        }
       }
 
-      const fileName = path.basename(env.path, path.extname(env.path))
-
-      // index.md はタイトルを表示しない
-      if (env.relativePath === 'index.md')
-        return
-
-      const hasH1 = tokens.some(token =>
-        token.type === 'heading_open' && token.tag === 'h1',
-      )
-
-      if (!hasH1) {
-        const title = env.frontmatter?.title || fileName
-        const h1Open = new Token('heading_open', 'h1', 1)
-        const h1Text = new Token('inline', '', 0)
-        const h1Close = new Token('heading_close', 'h1', -1)
-        h1Text.content = title
-        h1Text.children = []
-
-        tokens.unshift(h1Open, h1Text, h1Close)
-      }
-
-      // 2回目以降は処理しない（BiDirectionalLinks.getLink で [[]] ごとに markdown-it が呼び出される）
-      env.h1Handled = true
-    })
+      return originalRender(src, env)
+    }
   }
 }
 
