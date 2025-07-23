82fd47d154c0bfc15268e451e57812281bc07c7b
noy4 <naoyan418@gmail.com>
2025-07-23 20:13:38 +0900

    daisy exclude style

--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,3 @@
-import type { Preset } from 'unocss'
 import { presetDaisy } from '@ameinhardt/unocss-preset-daisy'
 import { defineConfig, presetIcons, presetWind4, transformerVariantGroup } from 'unocss'
 
@@ -19,16 +18,12 @@ export default defineConfig({
   ],
 })
 
-// presetDaisy は `.VPContent` 以下のみに適用（VitePressのスタイルとの競合を防ぐ）
-// ref. unocss > packages-presets/preset-wind4/src/postprocess/important.ts
-async function daisy(): Promise<Preset> {
-  return {
-    ...await presetDaisy(),
-    postprocess: (util) => {
-      // --un-text-opacity などは無視
-      if (util.layer === 'properties')
-        return
-      util.selector = `.VPContent > :not(.NotFound) ${util.selector}`
-    },
-  }
+// VitePress のスタイルと競合するクラスを除外
+async function daisy() {
+  const daisy = await presetDaisy()
+  const exclude = ['link', 'divider', 'menu']
+  daisy.rules = daisy.rules!.filter(rule =>
+    !exclude.some(e => rule[0].toString().includes(e)),
+  )
+  return daisy
 }
