6fa7a2203afb5e18bec186e848a7884503aae0eb
noy4 <naoyan418@gmail.com>
2025-07-31 23:39:30 +0900

    add terrain layer

--- docs/earth/maplibre-terrain/Earth.ts
+++ docs/earth/maplibre-terrain/Earth.ts
@@ -1,7 +1,24 @@
+import type { TerrainLayerProps } from '@deck.gl/geo-layers'
+import { TerrainLayer } from '@deck.gl/geo-layers'
+import { MapboxOverlay } from '@deck.gl/mapbox'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json'
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoibm95NCIsImEiOiJjbWRtczZmMzExcTMzMmtwdXUxcWZidG80In0.TgOdgcBfCZHIdcYqHk4nIw'
+const TERRAIN_IMAGE = `https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.png?access_token=${MAPBOX_TOKEN}`
+const SURFACE_IMAGE = `https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.png?access_token=${MAPBOX_TOKEN}`
+
+// https://docs.mapbox.com/help/troubleshooting/access-elevation-data/#mapbox-terrain-rgb
+// Note - the elevation rendered by this example is greatly exagerated!
+const ELEVATION_DECODER: TerrainLayerProps['elevationDecoder'] = {
+  rScaler: 6553.6,
+  gScaler: 25.6,
+  bScaler: 0.1,
+  offset: -10000,
+}
 
 export class Earth {
   init = () => {
@@ -19,7 +36,25 @@ export async function loadMap() {
     bearing: -27,
   })
 
+  const deckOverlay = new MapboxOverlay({
+    // interleaved: true,
+    layers: [
+      new TerrainLayer({
+        id: 'terrain',
+        minZoom: 0,
+        maxZoom: 23,
+        strategy: 'no-overlap',
+        elevationDecoder: ELEVATION_DECODER,
+        elevationData: TERRAIN_IMAGE,
+        texture: SURFACE_IMAGE,
+        wireframe: false,
+        color: [255, 255, 255],
+      }),
+    ],
+  })
+
   map.on('load', () => {
+    map.addControl(deckOverlay)
     map.addControl(new maplibregl.NavigationControl())
   })
 }
