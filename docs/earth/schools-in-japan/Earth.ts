9a624803da8250506aa017403c3e5c37ad38a1b6
noy4 <naoyan418@gmail.com>
2025-08-03 23:04:42 +0900

    light

--- docs/earth/schools-in-japan/Earth.ts
+++ docs/earth/schools-in-japan/Earth.ts
@@ -2,33 +2,13 @@ import type { Color, PickingInfo } from 'deck.gl'
 import { MapboxOverlay as DeckOverlay } from '@deck.gl/mapbox'
 import { load } from '@loaders.gl/core'
 import { CSVLoader } from '@loaders.gl/csv'
-import { AmbientLight, HexagonLayer, LightingEffect, PointLight } from 'deck.gl'
+import { HexagonLayer } from 'deck.gl'
 import maplibregl from 'maplibre-gl'
 import { withBase } from 'vitepress'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
 // Source data CSV
 const DATA_URL = withBase('/schools-japan-2013.csv')
-
-const ambientLight = new AmbientLight({
-  color: [255, 255, 255],
-  intensity: 1.0,
-})
-
-const pointLight1 = new PointLight({
-  color: [255, 255, 255],
-  intensity: 0.8,
-  position: [-0.144528, 49.739968, 80000],
-})
-
-const pointLight2 = new PointLight({
-  color: [255, 255, 255],
-  intensity: 0.8,
-  position: [-3.807751, 54.104682, 8000],
-})
-
-const lightingEffect = new LightingEffect({ ambientLight, pointLight1, pointLight2 })
-
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json'
 
 export const colorRange: Color[] = [
@@ -70,7 +50,6 @@ export async function loadEarth() {
 
   const deckOverlay = new DeckOverlay({
     // interleaved: true,
-    effects: [lightingEffect],
     getTooltip,
     layers: [
       new HexagonLayer<DataPoint>({
@@ -101,7 +80,6 @@ export async function loadEarth() {
   })
 
   map.on('load', () => {
-    // map.setProjection({ type: 'globe' });
     map.addControl(deckOverlay)
     map.addControl(new maplibregl.NavigationControl())
   })
