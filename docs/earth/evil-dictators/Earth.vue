15ac493b08c4e8d3149223a0ab692631e254932b
noy4 <naoyan418@gmail.com>
2025-08-15 08:01:22 +0900

    evil-dictators

---
+++ docs/earth/evil-dictators/Earth.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { GDPLocation } from './Earth'
+import { onMounted } from 'vue'
+import { Earth } from './Earth'
+import EarthCard from './EarthCard.vue'
+import locations from './gdp-per-capita.json'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+const earth = new Earth(locations as GDPLocation[])
+onMounted(earth.mount)
+</script>
+
+<template>
+  <div class="relative w-full h-screen z-1000">
+    <div id="cesiumContainer" class="w-full h-full" />
+    <EarthCard :earth />
+  </div>
+</template>
---
+++ docs/earth/evil-dictators/index.md
@@ -0,0 +1,13 @@
+---
+title: 邪悪な独裁者たち
+description: 多くの人々を死亡させた独裁者の地を訪れましょう。
+layout: page
+sidebar: false
+navbar: false
+---
+
+<script setup>
+import Earth from './Earth.vue'
+</script>
+
+<Earth />
