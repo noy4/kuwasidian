fb6d2746e954617c3b79450b4d91071757708dcc
noy4 <naoyan418@gmail.com>
2025-08-15 10:43:58 +0900

    column

--- docs/earth/evil-dictators/LocationItem.vue
+++ docs/earth/evil-dictators/LocationItem.vue
@@ -6,11 +6,25 @@ defineProps<{
   location: DictatorLocation
   index: number
 }>()
+
+function formatNumber(num: number): string {
+  if (num == null || Number.isNaN(num))
+    return ''
+  let result = ''
+  if (num >= 1_0000) {
+    result += `${Math.floor(num / 1_0000)}万`
+    num = num % 1_0000
+  }
+  if (num > 0 || result === '') {
+    result += num.toString()
+  }
+  return result
+}
 </script>
 
 <template>
   <button
-    class="text-sm transition-colors duration-100 px-2 py-1 rounded flex flex-wrap w-full text-start"
+    class="text-sm transition-colors duration-100 px-2 py-1 rounded w-full text-start"
     :class="[
       earth.currentLocationIndex.value === index
         ? 'text-white bg-white/20'
@@ -18,8 +32,20 @@ defineProps<{
     ]"
     @click="earth.goToLocation(index)"
   >
-    {{ index + 1 }}. {{ location.name }}（{{ location.country }}）
-    <div class="flex-1" />
-    <span class="ml-auto">{{ location.min_death_count.toLocaleString() }}</span>
+    {{ index + 1 }}. {{ location.name }}
+
+    <div class="flex">
+      <span>（{{ location.country }}）</span>
+      <span class="ml-auto">
+        {{
+          [
+            formatNumber(location.min_death_count),
+            location.min_death_count !== location.max_death_count
+              && `-${formatNumber(location.max_death_count)}`,
+            '人',
+          ].filter(Boolean).join('')
+        }}
+      </span>
+    </div>
   </button>
 </template>
