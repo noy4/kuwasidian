85464bd9c6d1d246d3004876a162660892d3d5f2
noy4 <naoyan418@gmail.com>
2025-08-16 11:35:25 +0900

    layout

--- docs/earth/evil-dictators/LocationItem.vue
+++ docs/earth/evil-dictators/LocationItem.vue
@@ -24,36 +24,38 @@ function formatNumber(num: number): string {
 
 <template>
   <button
-    class="text-sm transition-colors duration-100 px-2 py-1 rounded flex items-center text-start"
+    class="text-sm transition-colors duration-100 px-2 py-1 rounded flex text-start text-white"
     :class="[
       earth.currentLocationIndex.value === index
-        ? 'text-white bg-white/20'
-        : 'text-white/60 hover:bg-white/10 hover:text-white',
+        ? 'bg-white/20'
+        : 'opacity-60 hover:bg-white/10 hover:text-white',
     ]"
     @click="earth.goToLocation(index)"
   >
-    <div class="w-4">
-      {{ index + 1 }}.
-    </div>
-
-    <div class="w-9 h-9 bg-red rounded-full mr-2 relative overflow-hidden">
+    <div class="w-12 h-12 bg-red rounded mr-2 relative overflow-hidden">
       <img :src="location.icon" class="absolute inset-0 object-cover">
     </div>
 
     <div class="flex-1">
-      <div class="flex flex-wrap">
+      <div>
+        {{ index + 1 }}.
         <span class="font-semibold">{{ location.name }}</span>
-        <span>（{{ location.country }}）</span>
-        <span class="ml-auto">
-          {{
-            [
-              formatNumber(location.min_death_count),
-              location.min_death_count !== location.max_death_count
-                && `-${formatNumber(location.max_death_count)}`,
-              '人',
-            ].filter(Boolean).join('')
-          }}
-        </span>
+        （{{ location.country }}）
+      </div>
+
+      <div class="text-xs text-white/70">
+        {{ location.description }}
+      </div>
+
+      <div class="text-right">
+        {{
+          [
+            formatNumber(location.min_death_count),
+            location.min_death_count !== location.max_death_count
+              && `-${formatNumber(location.max_death_count)}`,
+            '人',
+          ].filter(Boolean).join('')
+        }}
       </div>
     </div>
   </button>
